9893f83d352a00ad8ac03b5689f0a33a701cfd29
sol0man <saihan.murtazaliev@yandex.ru>
2023-11-16 12:18:17 +0300

    Домашнее задание 9 спринта (исправленное)

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,16 +5,20 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 
 class SearchActivity : AppCompatActivity() {
+    var searchText: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         val backButton = findViewById<ImageButton>(R.id.back_button)
-        val clearButton = findViewById<ImageButton>(R.id.clearButton)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
 
         backButton.setOnClickListener{
@@ -22,11 +26,12 @@ class SearchActivity : AppCompatActivity() {
         }
         clearButton.setOnClickListener {
             searchEditText.text.clear()
-            clearButton.visibility = View.GONE
+            clearButton.isVisible = false
+            hideKeyboard(searchEditText)
         }
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                clearButton.isVisible = if (s.isNullOrEmpty()) false else true
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -35,21 +40,24 @@ class SearchActivity : AppCompatActivity() {
         })
 
         if (savedInstanceState != null) {
-            val searchText = savedInstanceState.getString("searchText")
+            searchText = savedInstanceState.getString("searchText").toString()
             searchEditText.setText(searchText)
         }
     }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
-        val searchText = searchEditText.text.toString()
+        searchText = searchEditText.text.toString()
         outState.putString("searchText", searchText)
     }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
-        val searchText = savedInstanceState.getString("searchText")
+        searchText = savedInstanceState.getString("searchText").toString()
         searchEditText.setText(searchText)
     }
-
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,29 +20,45 @@ class SettingsActivity : AppCompatActivity() {
         val shareButton = findViewById<ImageButton>(R.id.share_button)
         val supportButton = findViewById<ImageButton>(R.id.support_button)
         val agreementButton = findViewById<ImageButton>(R.id.agreement_button)
+        val darkModeSwitch = findViewById<Switch>(R.id.dark_mode)
 
         shareButton.setOnClickListener {
+            val urlShare = getString(R.string.url_share)
             val sendIntent = Intent()
             sendIntent.action = Intent.ACTION_SEND
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/profile/android-developer/")
+            sendIntent.putExtra(Intent.EXTRA_TEXT, urlShare)
             sendIntent.type = "text/plain"
 
             startActivity(sendIntent)
         }
         supportButton.setOnClickListener {
+            val emails = getString(R.string.emails_blank)
+            val forDevelopers = getString(R.string.for_developer)
+            val feedback = getString(R.string.feedback)
+
             val emailIntent = Intent(Intent.ACTION_SENDTO)
             emailIntent.data = Uri.parse("mailto:")
 
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("адрес_студента@example.com"))
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
-            emailIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emails))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, forDevelopers)
+            emailIntent.putExtra(Intent.EXTRA_TEXT, feedback)
 
             startActivity(emailIntent)
         }
         agreementButton.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            val urlAgreement = getString(R.string.url_agreement)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(urlAgreement))
 
             startActivity(browserIntent)
+        }
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Включить темную тему
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                // Включить светлую тему
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         }
     }
 }
